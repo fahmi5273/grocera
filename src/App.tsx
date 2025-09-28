84df8bc73a08b1c1da643607b101099d6d707efd
Fahmi <fahmi5273@gmail.com>
2025-09-29 01:51:22 +1000

    Update page.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,19 @@
 import { useState } from 'react';
 import reactLogo from './assets/react.svg';
-import viteLogo from '../public/vite.svg';
+import viteLogo from '/vite.svg';
 import './App.css';
 import { Button } from '@/components/ui/button';
+import {
+  Card,
+  CardAction,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -26,8 +37,46 @@ function App() {
       </div>
       <p className='read-the-docs'>Click on the Vite and React logos to learn more</p>
 
-      <div className='flex min-h-svh flex-col items-center justify-center'>
-        <Button>Click me</Button>
+      <div className='flex items-center justify-center mt-8'>
+        <Card className='w-full max-w-sm'>
+          <CardHeader>
+            <CardTitle>Login to your account</CardTitle>
+            <CardDescription>Enter your email below to login to your account</CardDescription>
+            <CardAction>
+              <Button variant='link'>Sign Up</Button>
+            </CardAction>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className='flex flex-col gap-6'>
+                <div className='grid gap-2'>
+                  <Label htmlFor='email'>Email</Label>
+                  <Input id='email' type='email' placeholder='m@example.com' required />
+                </div>
+                <div className='grid gap-2'>
+                  <div className='flex items-center'>
+                    <Label htmlFor='password'>Password</Label>
+                    <a
+                      href='#'
+                      className='ml-auto inline-block text-sm underline-offset-4 hover:underline'
+                    >
+                      Forgot your password?
+                    </a>
+                  </div>
+                  <Input id='password' type='password' required />
+                </div>
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter className='flex-col gap-2'>
+            <Button type='submit' className='w-full'>
+              Login
+            </Button>
+            <Button variant='outline' className='w-full'>
+              Login with Google
+            </Button>
+          </CardFooter>
+        </Card>
       </div>
     </>
   );
